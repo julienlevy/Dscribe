be4d1fbe0cc5d2db5f4952f787bf804e5ea5e5d2
Clement Joudet <clement.joudet@wanadoo.fr>
2015-11-28 18:01:07 +0000

    Adding search function by tag

--- emojizz/parse.py
+++ emojizz/parse.py
@@ -9,11 +9,16 @@ result = {}
 for element in data:
     result[element['unified']] = element['name']
 
+
+# TODO: find better tags
+# TODO: this script only work for unicode emojis with 5 chars in the unicode. A lot of emojis are missing !!
 stringResult = "["
 for key in result.keys():
-    if result[key]:
+    if result[key] and len(key) == 5:
         words = result[key].split()
-        stringResult += "\"U+" + key[:5] + "\":["
+        emoji = '\U000' + key[:5]
+        emoji = emoji.decode('unicode-escape')
+        stringResult += "\"" + emoji + "\":["
         for word in words:
             if word.lower() not in ['with', 'and', 'symbol']:
                 stringResult += "\"" + word.lower() + "\","
@@ -25,9 +30,11 @@ stringResult += "]"
 
 # sorted_tags = sorted(tags.items(), key=operator.itemgetter(1))
 # print(sorted_tags)
-
+print(stringResult)
 print("Opening result file")
 resultFile = open('emoji.txt', 'r+')
+resultFile.seek(0)
+resultFile.truncate()
 print("Writing result")
 resultFile.write(stringResult)
 print("All good.")
