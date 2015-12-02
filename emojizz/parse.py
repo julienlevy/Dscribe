e3fd9da3a186c0e827c49eb6c19efdd9dcc06e5f
Clement Joudet <clement.joudet@wanadoo.fr>
2015-12-02 19:33:01 +0000

    Score array

--- emojizz/parse.py
+++ emojizz/parse.py
@@ -13,6 +13,7 @@ for element in data:
 # TODO: find better tags
 # TODO: this script only work for unicode emojis with 5 chars in the unicode. A lot of emojis are missing !!
 stringResult = "["
+emojiArray = "["
 for key in result.keys():
     if result[key] and len(key) <= 5:
         words = result[key].split()
@@ -22,6 +23,7 @@ for key in result.keys():
             emoji = '\U0000' + key[:len(key)]
         emoji = emoji.decode('unicode-escape')
         stringResult += "\"" + emoji + "\":["
+        emojiArray += "\"" + emoji + "\":1,"
         for word in words:
             if word.lower() not in ['with', 'and', 'symbol']:
                 stringResult += "\"" + word.lower() + "\","
@@ -33,13 +35,17 @@ for key in result.keys():
 stringResult = stringResult[:-1]
 stringResult += "]"
 
+emojiArray = emojiArray[:-1]
+emojiArray += "]"
+
 # sorted_tags = sorted(tags.items(), key=operator.itemgetter(1))
 # print(sorted_tags)
-print(stringResult)
+# print(stringResult)
 print("Opening result file")
 resultFile = open('emoji.txt', 'r+')
 resultFile.seek(0)
 resultFile.truncate()
 print("Writing result")
-resultFile.write(stringResult)
+# resultFile.write(stringResult)
 print("All good.")
+print(emojiArray)
