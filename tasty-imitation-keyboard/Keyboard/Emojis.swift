c1349cfbf7a3c1099b336b9cc2389c1d8c3cc479
Clement Joudet <clement.joudet@wanadoo.fr>
2015-12-13 20:36:29 +0000

    Saving scores on disk

--- tasty-imitation-keyboard/Keyboard/Emojis.swift
+++ tasty-imitation-keyboard/Keyboard/Emojis.swift
@@ -8,20 +8,55 @@
 
 import Foundation
 
+var emojiScore = ["😍":1,"😎":1,"😏":1,"😊":1,"🔣":1,"🏸":1,"😔":1,"😆":1,"😵":1,"😴":1,"😳":1,"😲":1,"😱":1,"🔱":1,"💊":1,"🛃":1,"🔲":1,"😹":1,"😸":1,"🔳":1,"🔢":1,"🔴":1,"🗂":1,"🗃":1,"🗄":1,"📂":1,"®":1,"♉":1,"♈":1,"🔶":1,"✨":1,"🌘":1,"😁":1,"🌙":1,"♌":1,"♋":1,"♊":1,"♏":1,"♎":1,"♍":1,"🌔":1,"🌕":1,"🙂":1,"😷":1,"😾":1,"🗡":1,"😼":1,"😻":1,"😺":1,"😶":1,"🙈":1,"🙉":1,"🙊":1,"‼":1,"🙏":1,"🙁":1,"🇯":1,"🌌":1,"🌋":1,"🌊":1,"🚁":1,"⛪":1,"🌎":1,"🌍":1,"😐":1,"😰":1,"🔟":1,"🔝":1,"🔞":1,"🔛":1,"🔜":1,"🔚":1,"😮":1,"⏮":1,"🎖":1,"🎗":1,"🎐":1,"🎑":1,"🎒":1,"🎓":1,"👟":1,"👞":1,"👝":1,"👜":1,"🎙":1,"👚":1,"㊗":1,"3":1,"0":1,"⏯":1,"6":1,"7":1,"4":1,"5":1,"⏪":1,"8":1,"9":1,"㊙":1,"📨":1,"😒":1,"🍣":1,"🏓":1,"⏫":1,"🎞":1,"🎟":1];
 
-var emojiScore = ["🔣":1,"🏸":1,"😔":1,"😆":2,"😵":1,"😴":1,"😳":1,"😲":1,"😱":1,"🔱":1,"💊":1,"🛃":1,"🔲":1,"😹":1,"😸":1,"🔳":1,"🔢":1,"🔴":1,"🗂":1,"🗃":1,"🗄":1,"📂":1,"®":1,"♉":1,"♈":1,"🔶":1,"✨":1,"🌘":1,"😁":1,"🌙":1,"♌":1,"♋":1,"♊":1,"♏":1,"♎":1,"♍":1,"🌔":1,"🌕":1,"🙂":1,"😷":1,"😾":1,"🗡":1,"😼":1,"😻":1,"😺":1,"😶":1,"🙈":1,"🙉":1,"🙊":1,"‼":1,"🙏":1,"🙁":1,"🇯":1,"🌌":1,"🌋":1,"🌊":1,"🚁":1,"⛪":1,"🌎":1,"🌍":1,"😐":1,"😰":1,"🔟":1,"🔝":1,"🔞":1,"🔛":1,"🔜":1,"🔚":1,"😮":1,"⏮":1,"🎖":1,"🎗":1,"🎐":1,"🎑":1,"🎒":1,"🎓":1,"👟":1,"👞":1,"👝":1,"👜":1,"🎙":1,"👚":1,"㊗":1,"3":1,"0":1,"⏯":1,"6":1,"7":1,"4":1,"5":1,"⏪":1,"8":1,"9":1,"㊙":1,"📨":1,"😒":1,"🍣":1,"🏓":1,"⏫":1,"🎞":1,"🎟":1];
-
-
-class Emoji {
-    var emojiScore = ["😍":1,"😎":1,"😏":1,"😊":1,"🔣":1,"🏸":1,"😔":1,"😆":1,"😵":1,"😴":1,"😳":1,"😲":1,"😱":1,"🔱":1,"💊":1,"🛃":1,"🔲":1,"😹":1,"😸":1,"🔳":1,"🔢":1,"🔴":1,"🗂":1,"🗃":1,"🗄":1,"📂":1,"®":1,"♉":1,"♈":1,"🔶":1,"✨":1,"🌘":1,"😁":1,"🌙":1,"♌":1,"♋":1,"♊":1,"♏":1,"♎":1,"♍":1,"🌔":1,"🌕":1,"🙂":1,"😷":1,"😾":1,"🗡":1,"😼":1,"😻":1,"😺":1,"😶":1,"🙈":1,"🙉":1,"🙊":1,"‼":1,"🙏":1,"🙁":1,"🇯":1,"🌌":1,"🌋":1,"🌊":1,"🚁":1,"⛪":1,"🌎":1,"🌍":1,"😐":1,"😰":1,"🔟":1,"🔝":1,"🔞":1,"🔛":1,"🔜":1,"🔚":1,"😮":1,"⏮":1,"🎖":1,"🎗":1,"🎐":1,"🎑":1,"🎒":1,"🎓":1,"👟":1,"👞":1,"👝":1,"👜":1,"🎙":1,"👚":1,"㊗":1,"3":1,"0":1,"⏯":1,"6":1,"7":1,"4":1,"5":1,"⏪":1,"8":1,"9":1,"㊙":1,"📨":1,"😒":1,"🍣":1,"🏓":1,"⏫":1,"🎞":1,"🎟":1];
+class Emoji: NSObject, NSCoding {
+    var emojiScore: [String: Int]
+    var emojiTag: [String: [String]]
     
-    var emojiDict = ["😍":["smiling","face","heart-shaped","hearts","eyes"],"😎":["smiling","face","sunglasses"],"😏":["smirking","face"],"😊":["smiling","face","smiling","eyes"],"😋":["face","savouring","delicious","food"],"😌":["relieved","face"],"🕥":["clock","face","ten-thirty"],"🃏":["playing","card","black","joker"],"🕧":["clock","face","twelve-thirty"],"🕦":["clock","face","eleven-thirty"],"🕡":["clock","face","six-thirty"],"🕠":["clock","face","five-thirty"],"🕣":["clock","face","eight-thirty"],"🕢":["clock","face","seven-thirty"],"🦄":["unicorn","face"],"🚩":["triangular","flag","on","post"],"🔘":["radio","button"],"🎿":["ski","ski","boot"],"🎾":["tennis","racquet","ball"],"🎽":["running","shirt","sash"],"🎼":["musical","score"],"🎻":["violin"],"🎺":["trumpet"],"🕤":["clock","face","nine-thirty"],"🌊":["water","wave"],"🔑":["key"],"🌌":["milky","way"],"🌋":["volcano"],"🌎":["earth","globe","americas"],"🌍":["earth","globe","europe-africa"],"🔖":["bookmark"],"🌏":["earth","globe","asia-australia"],"🌉":["bridge","at","night"],"🌈":["rainbow"],"🎵":["musical","note"],"🎴":["flower","playing","cards"],"🎳":["bowling"],"🎲":["game","die"],"🎱":["billiards"],"🎰":["slot","machine"],"🌁":["foggy"],"🌀":["cyclone"],"🌃":["night","stars"],"🌂":["closed","umbrella"],"🌅":["sunrise"],"🌄":["sunrise","over","mountains"],"🌇":["sunset","over","buildings"],"🌆":["cityscape","at","dusk"],"🚺":["womens"],"🏣":["japanese","post","office"],"🏉":["rugby","football"],"😄":["smiling","face","open","mouth","smiling","eyes"],"😅":["smiling","face","open","mouth","cold","sweat"],"😆":["smiling","face","open","mouth","tightly-closed","eyes"],"😇":["smiling","face","halo"],"😀":["grinning","face"],"😁":["grinning","face","smiling","eyes"],"😂":["face","tears","of","joy"]]
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("emojis")
     
+    struct PropertyKey {
+        static let emojiScoreKey = "emojiScore"
+        static let emojiTagKey = "emojiTag"
+    }
+    
+    override init() {
+        self.emojiScore = ["😍":1,"😎":1,"😏":1,"😊":1,"🔣":1,"🏸":1,"😔":1,"😆":1,"😵":1,"😴":1,"😳":1,"😲":1,"😱":1,"🔱":1,"💊":1,"🛃":1,"🔲":1,"😹":1,"😸":1,"🔳":1,"🔢":1,"🔴":1,"🗂":1,"🗃":1,"🗄":1,"📂":1,"®":1,"♉":1,"♈":1,"🔶":1,"✨":1,"🌘":1,"😁":1,"🌙":1,"♌":1,"♋":1,"♊":1,"♏":1,"♎":1,"♍":1,"🌔":1,"🌕":1,"🙂":1,"😷":1,"😾":1,"🗡":1,"😼":1,"😻":1,"😺":1,"😶":1,"🙈":1,"🙉":1,"🙊":1,"‼":1,"🙏":1,"🙁":1,"🇯":1,"🌌":1,"🌋":1,"🌊":1,"🚁":1,"⛪":1,"🌎":1,"🌍":1,"😐":1,"😰":1,"🔟":1,"🔝":1,"🔞":1,"🔛":1,"🔜":1,"🔚":1,"😮":1,"⏮":1,"🎖":1,"🎗":1,"🎐":1,"🎑":1,"🎒":1,"🎓":1,"👟":1,"👞":1,"👝":1,"👜":1,"🎙":1,"👚":1,"㊗":1,"3":1,"0":1,"⏯":1,"6":1,"7":1,"4":1,"5":1,"⏪":1,"8":1,"9":1,"㊙":1,"📨":1,"😒":1,"🍣":1,"🏓":1,"⏫":1,"🎞":1,"🎟":1];
+        
+        self.emojiTag = ["😍":["smiling","face","heart-shaped","hearts","eyes"],"😎":["smiling","face","sunglasses"],"😏":["smirking","face"],"😊":["smiling","face","smiling","eyes"],"😋":["face","savouring","delicious","food"],"😌":["relieved","face"],"🕥":["clock","face","ten-thirty"],"🃏":["playing","card","black","joker"],"🕧":["clock","face","twelve-thirty"],"🕦":["clock","face","eleven-thirty"],"🕡":["clock","face","six-thirty"],"🕠":["clock","face","five-thirty"],"🕣":["clock","face","eight-thirty"],"🕢":["clock","face","seven-thirty"],"🦄":["unicorn","face"],"🚩":["triangular","flag","on","post"],"🔘":["radio","button"],"🎿":["ski","ski","boot"],"🎾":["tennis","racquet","ball"],"🎽":["running","shirt","sash"],"🎼":["musical","score"],"🎻":["violin"],"🎺":["trumpet"],"🕤":["clock","face","nine-thirty"],"🌊":["water","wave"],"🔑":["key"],"🌌":["milky","way"],"🌋":["volcano"],"🌎":["earth","globe","americas"],"🌍":["earth","globe","europe-africa"],"🔖":["bookmark"],"🌏":["earth","globe","asia-australia"],"🌉":["bridge","at","night"],"🌈":["rainbow"],"🎵":["musical","note"],"🎴":["flower","playing","cards"],"🎳":["bowling"],"🎲":["game","die"],"🎱":["billiards"],"🎰":["slot","machine"],"🌁":["foggy"],"🌀":["cyclone"],"🌃":["night","stars"],"🌂":["closed","umbrella"],"🌅":["sunrise"],"🌄":["sunrise","over","mountains"],"🌇":["sunset","over","buildings"],"🌆":["cityscape","at","dusk"],"🚺":["womens"],"🏣":["japanese","post","office"],"🏉":["rugby","football"],"😄":["smiling","face","open","mouth","smiling","eyes"],"😅":["smiling","face","open","mouth","cold","sweat"],"😆":["smiling","face","open","mouth","tightly-closed","eyes"],"😇":["smiling","face","halo"],"😀":["grinning","face"],"😁":["grinning","face","smiling","eyes"],"😂":["face","tears","of","joy"]]
+        super.init()
+    }
+    
+    init?(emojiScore: [String: Int], emojiTag: [String: [String]]) {
+        self.emojiScore = emojiScore
+        self.emojiTag = emojiTag
+        
+        super.init()
+        if emojiScore.isEmpty || emojiTag.isEmpty {
+            return nil
+        }
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(emojiScore, forKey: PropertyKey.emojiScoreKey)
+        aCoder.encodeObject(emojiTag, forKey: PropertyKey.emojiTagKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let emojiScore = aDecoder.decodeObjectForKey(PropertyKey.emojiScoreKey) as! [String: Int]
+        let emojiTag = aDecoder.decodeObjectForKey(PropertyKey.emojiTagKey) as! [String: [String]]
+        
+        self.init(emojiScore: emojiScore, emojiTag: emojiTag)
+    }
+    
+
     func tagSearch(sentence: String) -> [String] {
         let tagsArray = sentence.componentsSeparatedByString(" ");
         var result: [String: [Int]] = [String: [Int]](); //key=emoji, value=[Number of occurrences, score]
         for word in tagsArray {
-            for (key, tagArray) in emojiDict {
+            for (key, tagArray) in emojiTag {
                 for tag in tagArray {
                     if tag.rangeOfString(word) != nil {
                         if (result[key] != nil) {
@@ -60,17 +95,17 @@ class Emoji {
         return sortedKeys
     }
     
-    func getScoresFromMemory() -> [String: Int] {
-        //TODO: load scores from NSUSerDefaults
-        //let scores = NSUserDefaults.standardUserDefaults().objectForKey("scores") as! NSDictionary
-        let scores = self.emojiScore;
-        if (scores.isEmpty) {
-            //INIT WITH CONSTANT and store
-            let scores = ["😍":1,"😎":1,"😏":1,"😊":1,"🔣":1,"🏸":1,"😔":1,"😆":1,"😵":1,"😴":1,"😳":1,"😲":1,"😱":1,"🔱":1,"💊":1,"🛃":1,"🔲":1,"😹":1,"😸":1,"🔳":1,"🔢":1,"🔴":1,"🗂":1,"🗃":1,"🗄":1,"📂":1,"®":1,"♉":1,"♈":1,"🔶":1,"✨":1,"🌘":1,"😁":1,"🌙":1,"♌":1,"♋":1,"♊":1,"♏":1,"♎":1,"♍":1,"🌔":1,"🌕":1,"🙂":1,"😷":1,"😾":1,"🗡":1,"😼":1,"😻":1,"😺":1,"😶":1,"🙈":1,"🙉":1,"🙊":1,"‼":1,"🙏":1,"🙁":1,"🇯":1,"🌌":1,"🌋":1,"🌊":1,"🚁":1,"⛪":1,"🌎":1,"🌍":1,"😐":1,"😰":1,"🔟":1,"🔝":1,"🔞":1,"🔛":1,"🔜":1,"🔚":1,"😮":1,"⏮":1,"🎖":1,"🎗":1,"🎐":1,"🎑":1,"🎒":1,"🎓":1,"👟":1,"👞":1,"👝":1,"👜":1,"🎙":1,"👚":1,"㊗":1,"3":1,"0":1,"⏯":1,"6":1,"7":1,"4":1,"5":1,"⏪":1,"8":1,"9":1,"㊙":1,"📨":1,"😒":1,"🍣":1,"🏓":1,"⏫":1,"🎞":1,"🎟":1];
-            NSUserDefaults.standardUserDefaults().setObject(scores, forKey: "scores");
-        }
-        return scores;
-    }
+//    func getScoresFromMemory() -> [String: Int] {
+//        //TODO: load scores from NSUSerDefaults
+//        //let scores = NSUserDefaults.standardUserDefaults().objectForKey("scores") as! NSDictionary
+//        let scores = self.emojiScore;
+//        if (scores.isEmpty) {
+//            //INIT WITH CONSTANT and store
+//            let scores = ["😍":1,"😎":1,"😏":1,"😊":1,"🔣":1,"🏸":1,"😔":1,"😆":1,"😵":1,"😴":1,"😳":1,"😲":1,"😱":1,"🔱":1,"💊":1,"🛃":1,"🔲":1,"😹":1,"😸":1,"🔳":1,"🔢":1,"🔴":1,"🗂":1,"🗃":1,"🗄":1,"📂":1,"®":1,"♉":1,"♈":1,"🔶":1,"✨":1,"🌘":1,"😁":1,"🌙":1,"♌":1,"♋":1,"♊":1,"♏":1,"♎":1,"♍":1,"🌔":1,"🌕":1,"🙂":1,"😷":1,"😾":1,"🗡":1,"😼":1,"😻":1,"😺":1,"😶":1,"🙈":1,"🙉":1,"🙊":1,"‼":1,"🙏":1,"🙁":1,"🇯":1,"🌌":1,"🌋":1,"🌊":1,"🚁":1,"⛪":1,"🌎":1,"🌍":1,"😐":1,"😰":1,"🔟":1,"🔝":1,"🔞":1,"🔛":1,"🔜":1,"🔚":1,"😮":1,"⏮":1,"🎖":1,"🎗":1,"🎐":1,"🎑":1,"🎒":1,"🎓":1,"👟":1,"👞":1,"👝":1,"👜":1,"🎙":1,"👚":1,"㊗":1,"3":1,"0":1,"⏯":1,"6":1,"7":1,"4":1,"5":1,"⏪":1,"8":1,"9":1,"㊙":1,"📨":1,"😒":1,"🍣":1,"🏓":1,"⏫":1,"🎞":1,"🎟":1];
+//            NSUserDefaults.standardUserDefaults().setObject(scores, forKey: "scores");
+//        }
+//        return scores;
+//    }
     
     func incrementScore(emoji: String) -> Int {
         if self.emojiScore[emoji] != nil {
