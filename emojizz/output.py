75b3a7549e141a21777fee54e059552e7a662a56
Clement Joudet <clement.joudet@wanadoo.fr>
2016-01-03 16:51:42 +0100

    Generating plist with python

--- emojizz/output.py
+++ emojizz/output.py
@@ -1,15 +1,23 @@
 import json
+import plistlib
 
 with open('result.json') as data_file:
     data = json.load(data_file)
-    result = '['
-    for key, value in data.iteritems():
-        result += "\"" + key + "\": ["
-        for word in value:
-            result += "\"" + word.lower() + "\","
-        result = result[:-1]
-        result += '],'
-    result = result[:-1]
-    result += ']'
+    # result = '['
+    # for key, value in data.iteritems():
+    #     result += "\"" + key + "\": ["
+    #     for word in value:
+    #         result += "\"" + word.lower() + "\","
+    #     result = result[:-1]
+    #     result += '],'
+    # result = result[:-1]
+    # result += ']'
 
-print(result)
+print(data)
+with open('test.plist', 'w') as outfile:
+    try:
+        plistlib.writePlist(data, outfile)
+    finally:
+        outfile.close()
+
+# print(result)
