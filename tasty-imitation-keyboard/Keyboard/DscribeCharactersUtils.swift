bf1792ed001bd75268959cd33fce10dcedc42bd4
Julien Levy <julienlevy@ymail.com>
2016-01-07 20:15:14 +0100

    Right offset on keys for text to be centered

--- tasty-imitation-keyboard/Keyboard/DscribeCharactersUtils.swift
+++ tasty-imitation-keyboard/Keyboard/DscribeCharactersUtils.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 let smallPunctuation: [String] = [".", ",", ";", "*", ":", ";"]
+let bigOffset: [String] = ["{", "}", "[", "]", "+", "=", "\\", "/", "|", "<", ">", "-", ":", ";", "(", ")", "@"] //3 - 4.5 (Sketch)
+let mediumOffset: [String] = ["_", "•", ".", ","] //2-3
+let smallOffset: [String] = ["#", "%", "~", "€", "¥", "£", "*", "?", "!", "'", "$", "&", "\""] //1 - 2
 
 func keyLabelOffsetForCharacter(character: String, font: UIFont) -> CGFloat {
-    let offset: CGFloat = 0.8 * (font.capHeight - font.xHeight) * 2
-//    print(character)
-//    print(offset)
-    return 0
+    if bigOffset.contains(character) {
+        return 4.4
+    }
+    if mediumOffset.contains(character) {
+        return 2.0
+    }
+    if smallOffset.contains(character) {
+        return 1.3
+    }
+    if character == character.lowercaseString && Int(character) == nil {
+        return 0.7 * (font.capHeight - font.xHeight) * 2
+    }
+    return 1.0
 }
 
 func fontForKeyWithText(keyText: String, keytype: Key.KeyType) -> UIFont {
     if keytype == Key.KeyType.Character && keyText == keyText.lowercaseString {
         if #available(iOSApplicationExtension 8.2, *) {
-            return UIFont.systemFontOfSize(24, weight: UIFontWeightLight)
+            return UIFont.systemFontOfSize(24.5, weight: UIFontWeightLight)
         }
         return UIFont.systemFontOfSize(24)
     }
