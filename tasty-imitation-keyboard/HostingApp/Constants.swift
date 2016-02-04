f8153d828268cd85ecabf2a70439029ec181ffd9
Julien Levy <julienlevy@ymail.com>
2016-02-04 14:31:12 +0100

    Changed colors

--- tasty-imitation-keyboard/HostingApp/Constants.swift
+++ tasty-imitation-keyboard/HostingApp/Constants.swift
@@ -12,10 +12,12 @@ let onboardingIconWidth: CGFloat = 40
 
 extension UIColor {
     class func dscribeDarkOrange() -> UIColor {
-        return UIColor(red: 251.0/255, green: 183.0/255, blue: 59.0/255, alpha: 1.0)
+//        return UIColor(red: 251.0/255, green: 183.0/255, blue: 59.0/255, alpha: 1.0)
+        return UIColor(red: 254.0/255, green: 221.0/255, blue: 67.0/255, alpha: 1.0)
     }
     class func dscribeLightOrange() -> UIColor {
-        return UIColor(red: 254.0/255, green: 221.0/255, blue: 67.0/255, alpha: 1.0)
+//        return UIColor(red: 254.0/255, green: 221.0/255, blue: 67.0/255, alpha: 1.0)
+        return UIColor(red: 250.0/255, green: 155.0/255, blue: 7.0/255, alpha: 1.0)
     }
     class func dscribeOrangeText() -> UIColor {
         return UIColor(red: 250.0/255, green: 193.0/255, blue: 62.0/255, alpha: 1.0)
